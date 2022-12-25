ac4b913e3bc1025abe60f6dd5f249aec4b48dbac
santiago bohorquez <santibohorquez007@gmail.com>
2022-12-25 18:18:29 -0500

    finished

--- Jacksonville Data.R
+++ Jacksonville Data.R
@@ -1,9 +1,20 @@
 #Data Scraping Reviews for Bento Jacksonville data scraping
 #Santiago Bohorquez
 #12/19/2022
+
 library(data.table)
 library(rvest)
 library(tidyverse)
+
+
+
+
+
+################################################################################
+################################################################################
+
+
+
 
 url_1 = "https://www.yelp.com/biz/bento-asian-kitchen-sushi-jacksonville-4"
 
@@ -64,7 +75,15 @@ for (i in pageSequence_1) {
   df_new_table_1 <-  as.data.frame(df_new_1)
   
   df_final <- rbindlist(list(df_final, df_new_table_1))
+  Sys.sleep(60)
 }
+
+
+
+###############################################################################
+###############################################################################
+
+
 
 #Jacksonville Big Island Dr
 url_2 = "https://www.yelp.com/biz/bento-asian-kitchen-sushi-jacksonville-5"
@@ -112,7 +131,6 @@ for (i in pageSequence_2) {
     html_elements(xpath = "//div[starts-with(@class, ' review')]") %>%
     html_elements(xpath = "(.//p[starts-with(@class, 'comment')])[1]") %>%
     html_text() 
-  
   #turns my values into a list
   
   df_new_2 <- list( dates = dates_2,
@@ -122,6 +140,113 @@ for (i in pageSequence_2) {
   df_new_table_2 <-  as.data.frame(df_new_2)
   
   df_final <- rbindlist(list(df_final, df_new_table_2))
+  Sys.sleep(60)
 }
 
+
+
+
+
+
+###################################################################
+###################################################################
+
+
+
+
+#Jacksonville Beach Blvd
+url_3 = "https://www.yelp.com/biz/bento-asian-kitchen-and-sushi-jacksonville-3"
+
+page_3 <- read_html(url_3)
+
+#number of pages
+
+pageNums_3 <- page_3 %>%
+  html_elements(xpath = "//div[@aria-label='Pagination navigation']")%>%
+  html_text() %>%
+  str_extract('of \\d+') %>%
+  str_remove('of') %>%
+  as.numeric()
+
+#Sequence for pages
+pageSequence_3 <- seq(from = 0, to =10, by=10)
+
+#loop for code to run
+
+for (i in pageSequence_3) {
+  
+  url_3 = sprintf("https://www.yelp.com/biz/bento-asian-kitchen-and-sushi-jacksonville-3?start=%d",i)
+  
+  page_3 <- read_html(url_3)
+  #review date
+  
+  dates_3 <- page_3 %>%
+    html_elements(xpath = "//div[starts-with(@class, ' review')]") %>%
+    html_elements(xpath = "(.//span[@class = ' css-chan6m'])[1]") %>%
+    html_text() 
+  
+  #review rating
+  
+  review_ratings_3 <- page_3 %>%
+    html_elements(xpath = "//div[starts-with(@class, ' review')]") %>%
+    html_elements(xpath = "(.//div[contains(@aria-label, 'star rating')])[1]") %>%
+    html_attr("aria-label") %>%
+    str_remove_all(" star rating") %>%
+    as.numeric()
+  
+  # comments
+  
+  comment_3 <- page_3 %>%
+    html_elements(xpath = "//div[starts-with(@class, ' review')]") %>%
+    html_elements(xpath = "(.//p[starts-with(@class, 'comment')])[1]") %>%
+    html_text() 
+  #turns my values into a list
+  
+  df_new_3 <- list( dates = dates_3,
+                    reviews = review_ratings_3,
+                    comments = comment_3)
+  
+  df_new_table_3 <-  as.data.frame(df_new_3)
+  
+  df_final <- rbindlist(list(df_final, df_new_table_3))
+}
+
+
+
+##########################3
+url_4 = "https://www.yelp.com/biz/bento-asian-kitchen-and-sushi-jacksonville-3?start=10"
+
+page_4 <- read_html(url_4)
+dates_4 <- page_4 %>%
+  html_elements(xpath = "//div[starts-with(@class, ' review')]") %>%
+  html_elements(xpath = "(.//span[@class = ' css-chan6m'])[1]") %>%
+  html_text() 
+
+#review rating
+
+review_ratings_4 <- page_4 %>%
+  html_elements(xpath = "//div[starts-with(@class, ' review')]") %>%
+  html_elements(xpath = "(.//div[contains(@aria-label, 'star rating')])[1]") %>%
+  html_attr("aria-label") %>%
+  str_remove_all(" star rating") %>%
+  as.numeric()
+
+# comments
+
+comment_4 <- page_4 %>%
+  html_elements(xpath = "//div[starts-with(@class, ' review')]") %>%
+  html_elements(xpath = "(.//p[starts-with(@class, 'comment')])[1]") %>%
+  html_text() 
+#turns my values into a list
+
+df_new_4 <- list( dates = dates_4,
+                  reviews = review_ratings_4,
+                  comments = comment_4)
+
+df_new_table_4 <-  as.data.frame(df_new_4)
+beach_final <- rbindlist(list(df_new_table_4, df_new_table_3))
+################################
+df_final <- rbindlist(list(df_final, beach_final))
+
+################################
 write_csv(df_final, "Bento Jacksonville Yelp Review.csv", na = "")
